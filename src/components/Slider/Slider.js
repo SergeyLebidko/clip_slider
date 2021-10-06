dc1c604370ff6f807c201c165a535613b4df7172
SergeyLebidko <it.sergeyler@mail.ru>
2021-10-06 15:14:36 +0300

    Рефакторинг указания интервалов

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -3,33 +3,36 @@ import Slide from "../Slide/Slide";
 import {PATTERN_DATA, SLIDER_DATA} from "../../constants/settings";
 import "./Slider.scss";
 
+const BIG_INTERVAL = 3000;
+const SMALL_INTERVAL = 500;
+
 function Slider() {
     const [slide, setSlide] = useState(0);
     const [pattern, setPattern] = useState(0);
 
     const timer = useRef(null);
 
-    const getNextSlide = slide => slide === (SLIDER_DATA.length - 1) ? 0 : slide + 1;
+    const getNextSlideIndex = slideIndex => slideIndex === (SLIDER_DATA.length - 1) ? 0 : slideIndex + 1;
 
     function switchSlide(slideIndex, patternIndex) {
         if (patternIndex === (PATTERN_DATA.length - 1)) {
-            setSlide(getNextSlide(slideIndex));
+            setSlide(getNextSlideIndex(slideIndex));
             setPattern(0);
-            timer.current = setTimeout(() => switchSlide(getNextSlide(slideIndex), 0), 3000);
+            timer.current = setTimeout(() => switchSlide(getNextSlideIndex(slideIndex), 0), BIG_INTERVAL);
         } else {
-            setPattern(patternIndex + 1)
-            timer.current = setTimeout(() => switchSlide(slideIndex, patternIndex + 1), 500);
+            setPattern(patternIndex + 1);
+            timer.current = setTimeout(() => switchSlide(slideIndex, patternIndex + 1), SMALL_INTERVAL);
         }
     }
 
     useEffect(() => {
-        timer.current = setTimeout(() => switchSlide(slide, pattern), 3000);
+        timer.current = setTimeout(() => switchSlide(slide, pattern), BIG_INTERVAL);
         return () => clearTimeout(timer.current);
     }, []);
 
     return (
         <div className="slider">
-            <Slide {...SLIDER_DATA[getNextSlide(slide)]}/>
+            <Slide {...SLIDER_DATA[getNextSlideIndex(slide)]}/>
             <Slide {...SLIDER_DATA[slide]} pattern={PATTERN_DATA[pattern]}/>
         </div>
     );
