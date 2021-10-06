49799e61b1d3fad09b62f898f2134fa03a373a64
SergeyLebidko <it.sergeyler@mail.ru>
2021-10-06 11:55:57 +0300

    Создан первый вариант настроек

---
+++ src/constants/settings.js
@@ -0,0 +1,23 @@
+export const PATTERNS = [
+    [[0, 0], [100, 0], [100, 100], [0, 100]],
+    [[0, 50], [50, 50], [50, 0], [100, 0], [100, 100], [0, 100]],
+    [[0, 50], [100, 50], [100, 100], [0, 100]],
+    [[50, 50], [100, 50], [100, 100], [0, 100]]
+]
+
+export const PRESET1 = 'p1';
+export const PRESET2 = 'p2';
+export const PRESET3 = 'p3';
+export const PRESET4 = 'p4';
+export const PRESET5 = 'p5';
+
+export const SLIDE_DATA = [
+    {
+        preset: PRESET1,
+        mainHeader: '',
+        smallHeader: '',
+        text: '',
+        buttonText: '',
+        image: ''
+    }
+]
